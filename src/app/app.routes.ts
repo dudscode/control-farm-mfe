1061e5cf025402061bb88d5b1fd302009728b7e3
Lucas Coutinho <lucascgs1@hotmail.com>
2025-07-03 15:44:38 -0300

    feat: configuracao do material, ng-apexcharts e o roteamento para o app-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'vendas',
+    pathMatch: 'full',
+  },
+  {
+    path: 'vendas',
+    loadComponent: () =>
+      import('./core/pages/vendas/vendas.component').then(
+        (m) => m.VendasComponent
+      ),
+  },
+  {
+    path: 'producao',
+    loadComponent: () =>
+      import('./core/pages/producao/producao.component').then(
+        (m) => m.ProducaoComponent
+      ),
+  },
+  {
+    path: 'metas',
+    loadComponent: () =>
+      import('./core/pages/metas/metas.component').then(
+        (m) => m.MetasComponent
+      ),
+  },
+];
--- src/app/core/pages/vendas/vendas.component.ts
+++ src/app/core/pages/vendas/vendas.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexTitleSubtitle,
+  ApexXAxis,
+  NgApexchartsModule,
+  ApexOptions,
+} from 'ng-apexcharts';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+
+export type LucroProdutosChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
 
 @Component({
   selector: 'app-vendas',
-  imports: [],
+  imports: [CommonModule, NgApexchartsModule, MatCardModule],
   templateUrl: './vendas.component.html',
-  styleUrl: './vendas.component.scss'
+  styleUrl: './vendas.component.scss',
 })
 export class VendasComponent {
+  chartSeries: ApexAxisChartSeries = [
+    {
+      name: 'Vendas',
+      data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+    },
+  ];
+
+  chartDetails: ApexChart = {
+    type: 'line',
+    height: 350,
+  };
+
+  xAxis: ApexXAxis = {
+    categories: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set'],
+  };
+
+  chartTitle: ApexTitleSubtitle = {
+    text: 'Vendas Mensais',
+    align: 'left',
+  };
 
 }
