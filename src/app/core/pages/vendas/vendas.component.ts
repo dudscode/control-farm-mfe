adfc5b49eca1e21f9177cf0fe5e60f8d66f08999
Lucas Coutinho <lucascgs1@hotmail.com>
2025-07-05 03:49:52 -0300

    feat: criacao da pagina de vendas

--- src/app/core/pages/vendas/vendas.component.ts
+++ src/app/core/pages/vendas/vendas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   ApexAxisChartSeries,
   ApexChart,
@@ -6,24 +6,78 @@ import {
   ApexXAxis,
   NgApexchartsModule,
   ApexOptions,
+  ApexYAxis,
+  ApexPlotOptions,
+  ApexDataLabels,
+  ApexLegend,
 } from 'ng-apexcharts';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { DashboardCardComponent } from '../../../shared/components/dashboard-card/dashboard-card.component';
+import { AssetUrlPipe } from '../../../shared/pipes/asset-url.pipe';
 
-export type LucroProdutosChartOptions = {
+export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
   xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
   title: ApexTitleSubtitle;
+  plotOptions: ApexPlotOptions;
+  dataLabels: ApexDataLabels;
+  legend: ApexLegend;
 };
+
+interface DashboardCardData {
+  icon: string;
+  cardMetric: number;
+  efficiencyPercentage: number;
+  label: string;
+  isCurrency: boolean;
+  isPositive?: boolean;
+}
 
 @Component({
   selector: 'app-vendas',
-  imports: [CommonModule, NgApexchartsModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    NgApexchartsModule,
+    DashboardCardComponent,
+    AssetUrlPipe
+  ],
   templateUrl: './vendas.component.html',
   styleUrl: './vendas.component.scss',
 })
-export class VendasComponent {
+export class VendasComponent implements OnInit {
+  dashboardCards: DashboardCardData[] = [
+    {
+      icon: 'icon/cube-icon.svg',
+      cardMetric: 10293,
+      efficiencyPercentage: 1.3,
+      label: 'Total de produtos',
+      isCurrency: false,
+      isPositive: true,
+    },
+    {
+      icon: 'icon/finance-icon.svg',
+      cardMetric: -89000,
+      efficiencyPercentage: 4.2,
+      label: 'Total de vendas',
+      isCurrency: true,
+      isPositive: false,
+    },
+    {
+      icon: 'icon/person-icon.svg',
+      cardMetric: 40689,
+      efficiencyPercentage: 8.5,
+      label: 'Compradores',
+      isCurrency: false,
+      isPositive: false,
+    },
+  ];
+
   chartSeries: ApexAxisChartSeries = [
     {
       name: 'Vendas',
@@ -41,8 +95,12 @@ export class VendasComponent {
   };
 
   chartTitle: ApexTitleSubtitle = {
-    text: 'Vendas Mensais',
+    text: 'Lucro por Produto',
     align: 'left',
   };
 
+  ngOnInit(): void {
+
+
+  }
 }
